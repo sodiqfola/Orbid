a871254f41300a1c26d603f8e3daa484100e1f8a
Olanrewaju Folarin <olanrewajufolarin8@gmail.com>
2022-10-21 12:13:18 +0100

    third commit

--- js/anime.js
+++ js/anime.js
@@ -12,13 +12,13 @@ const sr = ScrollReveal({
 //
 sr.reveal(".header", { origin: "top" });
 sr.reveal(".hero-text-box", { delay: 200});
-sr.reveal(".hero-img", {delay: 600, distance: "70px" });
+sr.reveal(".hero-img", {delay: 500, distance: "70px" });
 // sr.reveal(".features", { delay: 200 });
-sr.reveal(".tp", { delay: 500, origin: "top" });
-sr.reveal(".tabs-bar", { delay: 700 });
-sr.reveal(".drop-grid", { delay: 700});
-sr.reveal(".creator-grid", { delay: 600 });
+sr.reveal(".tp", { delay: 300, origin: "top" });
+sr.reveal(".tabs-bar", { delay: 400 });
+sr.reveal(".drop-grid", { delay: 500});
+sr.reveal(".creator-grid", { delay: 500 });
 sr.reveal(".cta, .footer-grid", {delay: 500 });
-sr.reveal(".tp2", { delay: 600 });
-sr.reveal(".copyright", { delay: 900 });
+sr.reveal(".tp2", { delay: 300 });
+sr.reveal(".copyright", { delay: 700 });
 
